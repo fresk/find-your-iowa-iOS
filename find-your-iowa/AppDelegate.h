65ae009d8ae6b6be59424992380f014264a00e5d
Thomas Hansen <tehansen@Thomass-MacBook-Pro.local>
2013-12-09 09:07:34 -0600

    Initial Commit

---
+++ find-your-iowa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  find-your-iowa
+//
+//  Created by Thomas Hansen on 12/9/13.
+//  Copyright (c) 2013 Thomas Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
